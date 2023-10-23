f15f1257c2de7fa979ec06d0a8f424d841f6b45e
arromeral <138980560+arromeral@users.noreply.github.com>
2023-10-23 17:28:43 +0200

    Bonus_update

---
+++ your-code/update.sql
@@ -0,0 +1,13 @@
+update salesperson
+set store = 'Miami'
+where staff_ID = 00005;
+
+update customers
+set email = 'ppicasso@gmail.com'
+where customer_ID = 10001;
+update customers
+set email = 'lamarr_hd@hollywood.com'
+where customer_ID = 20001;
+update customers
+set email = 'hello@nasa.gov'
+where customer_ID = 30001;
