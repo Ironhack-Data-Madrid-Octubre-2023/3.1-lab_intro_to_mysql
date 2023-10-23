017f3803aa7fd40e9ea5dda1703e1bdf2023f38a
Ander <fernandezander1@gmail.com>
2023-10-23 18:58:49 +0200

    Finalizado

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE Salespersons
+SET Store = 'Miami'
+WHERE Name = 'Paige Turner';
+
+UPDATE Customers
+SET Email = 'ppicasso@gmail.com'
+WHERE Name = 'Pablo Picasso';
+
+UPDATE Customers
+SET Email = 'lamarr_hd@hollywood.com'
+WHERE Name = 'Hedy Lamarr';
+
+UPDATE Customers
+SET Email = 'hello@nasa.gov'
+WHERE Name = 'Katherine Johnson';
