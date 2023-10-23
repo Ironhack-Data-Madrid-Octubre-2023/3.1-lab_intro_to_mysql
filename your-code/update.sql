dd2c65220c78ed211983ebdad43473e20ff01cb2
Marina Luque García-Vaquero <77979161+maluque@users.noreply.github.com>
2023-10-23 17:35:53 +0200

    Update update.sql

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE Salespersons
+SET store_name = 'Miami'
+WHERE idstaff = 4;
+
+UPDATE Customers
+SET idcustomer = 0
+WHERE email = 'ppicasso@gmail.com';
+
+UPDATE Customers
+SET idcustomer = 1
+WHERE email = 'lamarr_hd@hollywood.com';
+
+UPDATE Customers
+SET idcustomer = 2
+WHERE email = 'hello@nasa.gov';
