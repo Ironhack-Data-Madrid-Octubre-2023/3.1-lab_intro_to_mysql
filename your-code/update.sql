f203f5dcc66a25121425f466c4ae2a7948509023
Marina Luque García-Vaquero <77979161+maluque@users.noreply.github.com>
2023-10-24 10:45:54 +0200

    Update update.sql

--- your-code/update.sql
+++ your-code/update.sql
@@ -1,15 +1,15 @@
-UPDATE Salespersons
-SET store_name = 'Miami'
-WHERE idstaff = 4;
+UPDATE cars_db.Salespersons
+SET store = 'Miami'
+WHERE idSales = 4;
 
-UPDATE Customers
-SET idcustomer = 0
+UPDATE cars_db.Customers
+SET idCustomers = 0
 WHERE email = 'ppicasso@gmail.com';
 
-UPDATE Customers
-SET idcustomer = 1
+UPDATE cars_db.Customers
+SET idCustomers = 1
 WHERE email = 'lamarr_hd@hollywood.com';
 
-UPDATE Customers
-SET idcustomer = 2
+UPDATE cars_db.Customers
+SET idCustomers = 2
 WHERE email = 'hello@nasa.gov';
