8d71eae031ab0df8eedb6c7eaa6dba1e76e19287
Noelia Roson <noelia.roson@gmail.com>
2023-10-24 13:24:41 +0200

    Terminado create

--- your-code/create.sql
+++ your-code/create.sql
@@ -18,43 +18,47 @@ CREATE SCHEMA IF NOT EXISTS `coches` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8m
 USE `coches` ;
 
 -- -----------------------------------------------------
--- Table `coches`.`customers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `coches`.`customers` (
-  `idcustomers` INT NOT NULL,
-  `customerid` CHAR(5) NULL,
-  `name` VARCHAR(45) NULL,
-  `phone` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `address` VARCHAR(45) NULL,
-  `city` VARCHAR(45) NULL,
-  `state_prov` VARCHAR(45) NULL,
-  `country` VARCHAR(45) NULL,
-  `postal` VARCHAR(45) NULL,
-  PRIMARY KEY (`idcustomers`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `coches`.`cars`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coches`.`cars` (
   `idcars` INT NOT NULL,
-  `vin` VARCHAR(45) NULL,
-  `manufacturer` VARCHAR(45) NULL,
-  `model` VARCHAR(45) NULL,
-  `year` YEAR(4) NULL,
-  `color` VARCHAR(45) NULL,
+  `vin` VARCHAR(45) NULL DEFAULT NULL,
+  `manufacturer` VARCHAR(45) NULL DEFAULT NULL,
+  `model` VARCHAR(45) NULL DEFAULT NULL,
+  `year` YEAR NULL DEFAULT NULL,
+  `color` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`idcars`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `coches`.`salesperson`
+-- Table `coches`.`customers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `coches`.`salesperson` (
+CREATE TABLE IF NOT EXISTS `coches`.`customers` (
+  `idcustomers` INT NOT NULL,
+  `customer_id` CHAR(5) NULL DEFAULT NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `phone` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `address` VARCHAR(45) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NULL DEFAULT NULL,
+  `state_prov` VARCHAR(45) NULL DEFAULT NULL,
+  `country` VARCHAR(45) NULL DEFAULT NULL,
+  `postal` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idcustomers`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `coches`.`salespersons`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `coches`.`salespersons` (
   `idsalesperson` INT NOT NULL,
-  `staffid` CHAR(5) NULL,
+  `staff_id` CHAR(5) NULL,
   `name` VARCHAR(45) NULL,
   `store` VARCHAR(45) NULL,
   PRIMARY KEY (`idsalesperson`))
@@ -66,31 +70,28 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coches`.`invoices` (
   `idinvoices` INT NOT NULL,
-  `inv_number` VARCHAR(45) NULL,
+  `invoice_number` VARCHAR(45) NULL,
   `date` DATE NULL,
-  `car` VARCHAR(45) NULL,
-  `customers` VARCHAR(45) NULL,
-  `salesperson` VARCHAR(45) NULL,
-  `customers_idcustomers` INT NOT NULL,
-  `cars_idcars` INT NOT NULL,
-  `salesperson_idsalesperson` INT NOT NULL,
-  PRIMARY KEY (`idinvoices`, `customers_idcustomers`, `cars_idcars`, `salesperson_idsalesperson`),
-  INDEX `fk_invoices_customers_idx` (`customers_idcustomers` ASC) VISIBLE,
-  INDEX `fk_invoices_cars1_idx` (`cars_idcars` ASC) VISIBLE,
-  INDEX `fk_invoices_salesperson1_idx` (`salesperson_idsalesperson` ASC) VISIBLE,
-  CONSTRAINT `fk_invoices_customers`
-    FOREIGN KEY (`customers_idcustomers`)
-    REFERENCES `coches`.`customers` (`idcustomers`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_cars1`
-    FOREIGN KEY (`cars_idcars`)
+  `idcars` INT NULL,
+  `idcustomers` INT NULL,
+  `idsalesperson` INT NULL,
+  PRIMARY KEY (`idinvoices`),
+  INDEX `idcars_idx` (`idcars` ASC) VISIBLE,
+  INDEX `idcustomers_idx` (`idcustomers` ASC) VISIBLE,
+  INDEX `idsalesperson_idx` (`idsalesperson` ASC) VISIBLE,
+  CONSTRAINT `idcars`
+    FOREIGN KEY (`idcars`)
     REFERENCES `coches`.`cars` (`idcars`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_salesperson1`
-    FOREIGN KEY (`salesperson_idsalesperson`)
-    REFERENCES `coches`.`salesperson` (`idsalesperson`)
+  CONSTRAINT `idcustomers`
+    FOREIGN KEY (`idcustomers`)
+    REFERENCES `coches`.`customers` (`idcustomers`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `idsalesperson`
+    FOREIGN KEY (`idsalesperson`)
+    REFERENCES `coches`.`salespersons` (`idsalesperson`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
