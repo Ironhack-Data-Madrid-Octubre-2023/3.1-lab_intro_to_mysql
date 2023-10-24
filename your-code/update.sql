24e372b5b014bb539576f085129956534dfd732a
Jacobo Mate <mateblanquerjacobo@gmail.com>
2023-10-24 11:13:40 +0200

    Updates bien hechos!

--- your-code/update.sql
+++ your-code/update.sql
@@ -2,6 +2,19 @@ update customers
 set email = 'ppicasso@gmail.com'
 where name = 'Pablo Picasso'; 
 
+update customers
+set email = 'ppicasso@gmail.com'
+where name = 'Pablo Picasso'; 
+
+update customers
+set email = 'lamarr_hd@hollywood.com'
+where name = 'Hedy Lamarr'; 
+
+update customers
+set email = 'hello@nasa.gov'
+where name = 'Katherine Johnson'; 
+
+
 UPDATE salespersons
 SET store = 'Miami'
 WHERE name = 'Paige Turner';
