8a08746ca4ec14ac03c56d25d31b59765f121fac
Kohkitos <sanzyagain@gmail.com>
2023-10-24 12:34:27 +0200

    Bonus: update added

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE salesperson
+SET store='Miami'
+WHERE id_salesperson=4;
+
+UPDATE customer
+SET email='ppicasso@gmail.com'
+WHERE id_customer=0;
+
+UPDATE customer
+SET email='lamarr_hd@hollywood.com'
+WHERE id_customer=1;
+
+UPDATE customer
+SET email='hello@nasa.gov'
+WHERE id_customer=2;
