fb0e7ad20af59144c170ae3a83eb7185cf3ec3d5
Marina Luque García-Vaquero <77979161+maluque@users.noreply.github.com>
2023-10-24 10:42:46 +0200

    Update seeding.sql

--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -1,5 +1,4 @@
-
-INSERT INTO Cars
+INSERT INTO cars_db.Cars
 VALUES
     (0, '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue'),
     (1, 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red'),
@@ -8,14 +7,14 @@ VALUES
     (4, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
     (5, 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
     
-INSERT INTO Customers
+INSERT INTO cars_db.Customers
 VALUES
 	(0, 10001, 	'Pablo Picasso' ,'34 636 17 63 82', '-' ,'Paseo de la Chopera, 14' ,	'Madrid' ,'Madrid' ,'Spain' ,28045),
 	(1, 20001, 	'Hedy Lamarr' 	,'43 1 514442250', '-'  	, 'Weiglgasse 10' ,	'Viena', 'Vienna' ,'Austria' ,	1150),
 	(2, 30001, 	'Katherine Johnson' ,'1 202 358 0000', '-'  ,'300 E St SW' ,'Washington, DC', 'Washington' ,'United States' ,20546);
 
 
-INSERT INTO Salespersons
+INSERT INTO cars_db.Salespersons
 VALUES
     (0, '00001', 'Petey Cruiser', 'Madrid'),
     (1, '00002', 'Anna Sthesia', 'Barcelona'),
@@ -27,7 +26,7 @@ VALUES
     (7, '00008', 'Shonda Leer', 'São Paulo');
 
 
-INSERT INTO Invoices
+INSERT INTO cars_db.Invoices
 VALUES
     (0, 852399038, '2018-08-22', 0, 1, 3),
     (1, 731166526, '2018-12-31', 3, 0, 5),
