6659413c889c6893124adbe309d0cc52275f7846
LeoBulcsu <136447924+LeoBulcsu@users.noreply.github.com>
2023-10-24 10:23:17 +0200

    Update create.sql

--- your-code/create.sql
+++ your-code/create.sql
@@ -5,32 +5,23 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema lab_mysql
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-
-USE `lab_mysql` ;
-
 -- -----------------------------------------------------
--- Table `lab_mysql`.`CARS`
+-- Schema lab_sql_cars
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`CARS` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `VIN` VARCHAR(45) NOT NULL,
-  `Manufacturer` VARCHAR(45) NULL,
-  `Model` VARCHAR(45) NULL,
-  `Year` INT NULL,
-  `Color` VARCHAR(45) NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE INDEX `VIN_UNIQUE` (`VIN` ASC) VISIBLE)
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`Customers`
+-- Schema lab_sql_cars
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`Customers` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
+CREATE SCHEMA IF NOT EXISTS `lab_sql_cars` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `lab_sql_cars` ;
+
+-- -----------------------------------------------------
+-- Table `lab_sql_cars`.`Customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_sql_cars`.`Customers` (
+  `ID` INT NOT NULL,
   `Customer ID` INT NOT NULL,
   `Name` VARCHAR(45) NULL,
   `Phone` VARCHAR(45) NULL,
@@ -41,53 +32,65 @@ CREATE TABLE IF NOT EXISTS `lab_mysql`.`Customers` (
   `Country` VARCHAR(45) NULL,
   `Postal` INT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE INDEX `Customer ID_UNIQUE` (`Customer ID` ASC) VISIBLE,
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE,
-  UNIQUE INDEX `Phone_UNIQUE` (`Phone` ASC) VISIBLE)
+  INDEX `Customer ID` (`Customer ID` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`Salesperson`
+-- Table `lab_sql_cars`.`Cars`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`Salesperson` (
+CREATE TABLE IF NOT EXISTS `lab_sql_cars`.`Cars` (
   `ID` INT NOT NULL,
-  `Staff ID` INT NOT NULL AUTO_INCREMENT,
+  `VIN` VARCHAR(45) NOT NULL,
+  `Manufacturer` VARCHAR(45) NULL,
+  `Model` VARCHAR(45) NULL,
+  `Year` INT NULL,
+  `Color` VARCHAR(45) NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `VIN` (`VIN` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab_sql_cars`.`Salesperson`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_sql_cars`.`Salesperson` (
+  `ID` INT NOT NULL,
+  `StaffID` INT NOT NULL,
   `Name` VARCHAR(45) NULL,
   `Store` VARCHAR(45) NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE INDEX `Staff ID_UNIQUE` (`Staff ID` ASC) VISIBLE)
+  INDEX `Staff ID` (`StaffID` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`Invoices`
+-- Table `lab_sql_cars`.`Invoices`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`Invoices` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `Invoice Number` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `lab_sql_cars`.`Invoices` (
+  `ID` INT NOT NULL,
+  `Invoice Number` INT NULL,
   `Date` DATE NULL,
-  `Car` INT NOT NULL,
-  `Customer` INT NOT NULL,
-  `Sales Person` INT NOT NULL,
-  PRIMARY KEY (`ID`, `Car`, `Sales Person`, `Customer`),
+  `Car` INT NULL,
+  `Customer` INT NULL,
+  `Sales Person` INT NULL,
+  PRIMARY KEY (`ID`),
   INDEX `Customer ID fk1_idx` (`Customer` ASC) VISIBLE,
-  INDEX `Car ID_idx` (`Car` ASC) VISIBLE,
-  INDEX `SalesPerson ID_idx` (`Sales Person` ASC) VISIBLE,
-  UNIQUE INDEX `Invoice Number_UNIQUE` (`Invoice Number` ASC) VISIBLE,
+  INDEX `Car ID fk2_idx` (`Car` ASC) VISIBLE,
+  INDEX `Sales Person ID fk3_idx` (`Sales Person` ASC) VISIBLE,
   CONSTRAINT `Customer ID fk1`
     FOREIGN KEY (`Customer`)
-    REFERENCES `lab_mysql`.`Customers` (`Customer ID`)
+    REFERENCES `lab_sql_cars`.`Customers` (`Customer ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `Car ID`
+  CONSTRAINT `Car ID fk2`
     FOREIGN KEY (`Car`)
-    REFERENCES `lab_mysql`.`CARS` (`ID`)
+    REFERENCES `lab_sql_cars`.`Cars` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `SalesPerson ID`
+  CONSTRAINT `Sales Person ID fk3`
     FOREIGN KEY (`Sales Person`)
-    REFERENCES `lab_mysql`.`Salesperson` (`Staff ID`)
+    REFERENCES `lab_sql_cars`.`Salesperson` (`StaffID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
