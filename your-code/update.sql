bf4d7785ee12c47bb391fa7ffd3c7a959d4471b1
LeoBulcsu <136447924+LeoBulcsu@users.noreply.github.com>
2023-10-24 11:23:16 +0200

    Update update.sql

---
+++ your-code/update.sql
@@ -0,0 +1,22 @@
+select *
+from Salesperson;
+
+update Salesperson
+set  Store = 'Miami'
+where id = 4;
+
+select *
+from Customers;
+
+update Customers
+set  Email = 'ppicasso@gmail.com'
+where id = 0;
+
+update Customers
+set  Email = 'lamarr_hd@hollywood.com'
+where id = 1;
+
+update Customers
+set  Email = 'hello@nasa.gov'
+where id = 2;
+
